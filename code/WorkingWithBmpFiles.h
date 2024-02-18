c989c9c294f87758fca1ccba8dbcf2461fd9cb4f
Aleksandr Erkhov <sasori654654@gmail.com>
2024-02-18 13:23:44 +0300

    create class2

--- code/WorkingWithBmpFiles.h
+++ code/WorkingWithBmpFiles.h
@@ -2,7 +2,8 @@
 #define BMP_FILES_PVM
 
 class BMPfile;
-//void BMPfile::write_bmp(const char* nameFile);
-//void BMPfile::changePixel(unsigned int positionM, unsigned int positionN, unsigned char size);
+BMPfile::BMPfile(unsigned int weight, unsigned int height);
+void BMPfile::write_bmp(const char* nameFile);
+void BMPfile::changePixel(unsigned int positionM, unsigned int positionN, unsigned char size) const;
 
 #endif
